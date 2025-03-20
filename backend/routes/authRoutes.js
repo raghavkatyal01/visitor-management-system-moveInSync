e9af35d3a86f90d7ae896ebe1d4314ff6e772ed3
RAGHAV KATYAL <katyalraghav897@gmail.com>
2025-03-21 02:09:38 +0530

    Implemention done of user authentication  using JWT

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,69 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+};
+
+
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hashedPassword, role });
+
+    if (user) {
+      res.status(201).json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        token: generateToken(user.id),
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        token: generateToken(user.id),
+      });
+    } else {
+      res.status(401).json({ message: "Invalid email or password" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/profile", protect, async (req, res) => {
+  res.json(req.user);
+});
+
+export default router;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import connectDB from "../config/db.js";
+import connectDB from '../config/db.js'
+import "../models/User.js";
+import "../models/Visitor.js";
+import "../models/VisitRequest.js";
+import "../models/Photo.js";
+import authRoutes from "../routes/authRoutes.js";
 
-dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+dotenv.config();
 connectDB();
 
 app.get("/", (req, res) => {
   res.json({ message: "Visitor Management System API is running..." });
 });
-
+app.use("/api/auth", authRoutes);
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
